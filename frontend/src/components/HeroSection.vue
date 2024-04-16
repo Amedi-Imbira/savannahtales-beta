e873d3d5dc2643e8c824e06b55ffd80f207d096e
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-04-16 11:11:32 +0300

    ✅completed navbar styling

---
+++ frontend/src/components/HeroSection.vue
@@ -0,0 +1,22 @@
+<template>
+      <div class="banner">
+            <h1>What value does Savannah Tales provide?</h1>
+            <h3>How can we help you?</h3>
+            <a href="#">start reading</a>
+      </div>
+      <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto amet reiciendis vero aspernatur esse dolores, mollitia expedita, dolor harum eius nihil quidem accusantium perferendis in ea officia optio animi repellendus.
+            
+      </div>
+</template>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+      <div class="nav">
+            <a class="brand-logo" href="#">savannah tales</a>
+            <ul class="nav-links">
+                  <li><a href="#">our story</a></li>
+                  <li><a href="#">write for us</a></li>
+                  <li><a href="#">log in</a></li>
+            </ul>
+            <a class="cta" href="#">get started</a>
+      </div>
+</template>
