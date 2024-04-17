961aedb53847e0815256f4f4ef38046a3d18aff0
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-04-17 10:41:24 +0300

    ✅Finished the mockup design of landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,11 +2,15 @@
 import Navbar from './components/Navbar.vue';
 import HeroSection from './components/HeroSection.vue';
 import Features from './components/Features.vue';
+import ValueProposition from './components/ValueProposition.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Navbar />
   <HeroSection />
   <Features />
+  <ValueProposition />
+  <Footer />
 
 </template>
